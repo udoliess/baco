546900f021fa499ba38f40d3331fd843f8453133
Udo Liess <udo.liess@gmx.net>
2014-02-01 22:50:21 +0100

    Rewrite Walk.Deep(). DirectoryInfo.EnumerateFiles() does not return hidden files under linux! But DirectoryInfo.EnumerateFileSystemInfos() does return all entries.

--- baco/Walk.cs
+++ baco/Walk.cs
@@ -17,21 +17,30 @@ namespace baco
 
 		static void Go(string path, string relative, Regex include, Regex exclude, Action<string> handleDir, Action<string> handleFile)
 		{
-			var p = Path.Combine(path, relative);
-			foreach (var d in Directories.All(p, include, exclude))
+			foreach (var fsi in new DirectoryInfo(Path.Combine(path, relative)).EnumerateFileSystemInfos("*", SearchOption.TopDirectoryOnly))
 			{
-				var pr = Path.Combine(relative, d);
-				handleDir(pr);
-				Go(path, pr, include, exclude, handleDir, handleFile);
+				if ((fsi.Attributes & FileAttributes.Directory) == 0)
+					if (Filter.Where(fsi.FullName, include, exclude))
+						handleFile(Path.Combine(relative, fsi.Name));
+				else
+				{
+					var s = Path.Combine(fsi.FullName, "*");
+					if (Filter.Where(s.Substring(0, s.Length - 1), include, exclude))
+					{
+						var pr = Path.Combine(relative, fsi.Name);
+						handleDir(pr);
+						Go(path, pr, include, exclude, handleDir, handleFile);
+					}
+				}
 			}
-			foreach (var f in Files.All(p, include, exclude))
-				handleFile(Path.Combine(relative, f));
 		}
 
 		static void Go(string directory, string pattern, Regex include, Regex exclude, Action<string> handleFile)
 		{
-			foreach (var f in Files.All(directory, pattern, include, exclude))
-				handleFile(f);
+			foreach (var fsi in new DirectoryInfo(directory).EnumerateFileSystemInfos(pattern, SearchOption.TopDirectoryOnly))
+				if ((fsi.Attributes & FileAttributes.Directory) == 0)
+					if (Filter.Where(fsi.FullName, include, exclude))
+						handleFile(fsi.Name);
 		}
 	}
 }
