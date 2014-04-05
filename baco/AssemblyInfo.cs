6035881c0025ed7aa9654439ac66002869078d6a
Udo Liess <udo.liess@gmx.net>
2014-04-05 10:25:02 +0200

    set version to 2.1.1.0

--- baco/AssemblyInfo.cs
+++ baco/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.CompilerServices;
 // Change them to the values specific to your project.
 
 [assembly: AssemblyTitle("baco")]
-[assembly: AssemblyDescription("baco (BackupCopy), backup using copy and hard links, www.udol.de/baco")]
+[assembly: AssemblyDescription("baco (BackupCopy), backup using copy and hard links, <udol.de/baco>")]
 [assembly: AssemblyConfiguration("Mono / .NET 4.0")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("")]
@@ -17,7 +17,7 @@ using System.Runtime.CompilerServices;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("2.1.0.3")]
+[assembly: AssemblyVersion("2.1.1.0")]
 
 // The following attributes are used to specify the signing key for the assembly, 
 // if desired. See the Mono documentation for more information about signing.
