365342d4f659fcbc7b05f09e6a58222d66939a27
Udo Liess <udo.liess@gmx.net>
2014-09-10 20:23:25 +0200

    use FileInfo

--- baco/Attributes.cs
+++ baco/Attributes.cs
@@ -7,14 +7,14 @@ namespace baco
 	{
 		public static void Copy(string source, string destination)
 		{
+			var s = new FileInfo(source);
+			var d = new FileInfo(destination);
 			var copy = new Action(() =>
 				{
-					var sourceLastWriteTimeUtc = File.GetLastWriteTimeUtc(source);
-					if (File.GetLastWriteTimeUtc(destination) != sourceLastWriteTimeUtc)
-						File.SetLastWriteTimeUtc(destination, sourceLastWriteTimeUtc);
-					var sourceCreationTimeUtc = File.GetCreationTimeUtc(source);
-					if (File.GetCreationTimeUtc(destination) != sourceCreationTimeUtc)
-						File.SetCreationTimeUtc(destination, sourceCreationTimeUtc);
+					if (d.CreationTimeUtc != s.CreationTimeUtc)
+						d.CreationTimeUtc = s.CreationTimeUtc;
+					if (d.LastWriteTimeUtc != s.LastWriteTimeUtc)
+						d.LastWriteTimeUtc = s.LastWriteTimeUtc;
 				});
 			try
 			{
@@ -24,7 +24,7 @@ namespace baco
 			{
 				try
 				{
-					File.SetAttributes(destination, FileAttributes.Normal);
+					d.Attributes = FileAttributes.Normal;
 					copy();
 				}
 				catch
